3f17cd2ba38606991e3b90771b80aeabbf486a5c
Philipp Bayer <philippbay@gmail.com>
2013-09-09 21:15:34 +1000

    Links are fixed now.... I think.

--- app/models/curated_phenotype_answer.rb
+++ app/models/curated_phenotype_answer.rb
@@ -1,5 +1,6 @@
 class CuratedPhenotypeAnswer < ActiveRecord::Base
   belongs_to :curated_phenotype
+  has_many :curated_user_phenotypes
 
   attr_accessible :variation_name
 end
---
+++ app/models/curated_user_phenotype.rb
@@ -0,0 +1,4 @@
+class CuratedUserPhenotype < ActiveRecord::Base
+  attr_accessible :belongs_to, :belongs_to, :belongs_to, :time_of_data_point, :comment
+  belongs_to :curated_user_phenotype_answers
+end
