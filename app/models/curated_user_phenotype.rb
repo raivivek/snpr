8e0587e36062986fe6fa21afdd03e504e564dfbf
Philipp Bayer <philippbay@gmail.com>
2013-09-15 21:14:50 +1000

    Adds more details around curated user phenotypes creation - unsure how to do forms

--- app/models/curated_user_phenotype.rb
+++ app/models/curated_user_phenotype.rb
@@ -1,4 +1,4 @@
 class CuratedUserPhenotype < ActiveRecord::Base
-  attr_accessible :belongs_to, :time_of_data_point, :comment, :is_multiple, :is_series
+  attr_accessible :belongs_to, :time_of_data_point, :comment
   belongs_to :curated_user_phenotype_answers
 end
