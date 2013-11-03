9601d3fda94105f3b81b3a00b484fdefec60e59a
Philipp Bayer <philippbay@gmail.com>
2013-11-03 14:51:04 +1000

    Fixes a small bug in user-phenotype relations

--- app/models/curated_user_phenotype.rb
+++ app/models/curated_user_phenotype.rb
@@ -1,4 +1,5 @@
 class CuratedUserPhenotype < ActiveRecord::Base
-  attr_accessible :belongs_to, :time_of_data_point, :comment
-  belongs_to :curated_user_phenotype_answers
+  attr_accessible :belongs_to, :time_of_data_point, :comment, :user_id, :curated_phenotype_id, :curated_phenotype_answer_id
+  belongs_to :user
+  belongs_to :curated_phenotype
 end
