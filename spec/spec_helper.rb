76629021548519ac1daa2205bbb7771ffce3170b
Philipp Bayer <PhilippBay@gmail.com>
2015-04-05 14:51:32 +0200

    Revert "Fix messaging"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require 'rspec/rails'
 require 'sidekiq/testing'
 require 'factory_girl_rails'
 require 'pry-rails' unless ENV['CI']
-require "authlogic/test_case"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
